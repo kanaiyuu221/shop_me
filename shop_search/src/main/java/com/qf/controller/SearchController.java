f75c75840d56e11df457023e193652deeb62d608
kanaiyuu <shimakaze221@outlook.com>
2019-01-19 16:21:28 +0800

    实现显示搜索结果

--- shop_search/src/main/java/com/qf/controller/SearchController.java
+++ shop_search/src/main/java/com/qf/controller/SearchController.java
@@ -1,9 +1,13 @@
 package com.qf.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.qf.entity.Goods;
 import com.qf.service.ISearchService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/search")
@@ -17,9 +21,10 @@ public class SearchController {
      * 关键字查询
      */
     @RequestMapping("/query")
-    public String search(String keyword){
+    public String search(String keyword,Model model){
         System.out.println("搜索关键字是>>>>> "+keyword);
-
+        List<Goods> goodsList = searchService.queryByIndexed(keyword);
+        model.addAttribute("goodslist",goodsList);
 
         return "searchlist";
     }
--- shop_service/shop_search_service/src/main/java/com/qf/impl/SearchServiceImpl.java
+++ shop_service/shop_search_service/src/main/java/com/qf/impl/SearchServiceImpl.java
@@ -4,11 +4,16 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.qf.entity.Goods;
 import com.qf.service.ISearchService;
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,7 +24,32 @@ public class SearchServiceImpl implements ISearchService {
 
     @Override
     public List<Goods> queryByIndexed(String keyword) {
-        return null;
+        //查询索引库返回预览结果 不查询数据库
+        SolrQuery solrQuery=new SolrQuery();
+        if (keyword == null){
+            solrQuery.setQuery("*:*");
+        }else {
+            solrQuery.setQuery("gtitle:"+keyword+"|| ginfo:"+keyword);
+        }
+        List<Goods> goods=new ArrayList<>();
+        try {
+            QueryResponse queryResponse=solrClient.query(solrQuery);
+            SolrDocumentList solr=queryResponse.getResults();
+            for (SolrDocument entries : solr) {
+                int id= Integer.parseInt((String) entries.get("id"));
+                String gtitle = (String) entries.get("gtitle");
+                String ginfo = (String) entries.get("ginfo");
+                float gprice = (float) entries.get("gprice");
+                int gcount = (int) entries.get("gcount");
+                String gimage = (String) entries.get("gimage");
+                Goods goods1=new Goods(id,gtitle,ginfo,gcount,0,0,gprice,gimage);
+                goods.add(goods1);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return goods;
     }
 
     /**
